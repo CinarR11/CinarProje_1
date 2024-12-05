3a7a2ceba6452223ecbbc5171a2fa4d146d7612f
CinarR <profilo12bcinar@gmail.com>
2024-12-05 11:32:21 +0300

    Add files via upload

---
+++ 1328_12B/amp12b_1083_stp/Model/Musteriler.cs
@@ -0,0 +1,24 @@
+﻿using amp12b_1083_stp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace amp12b_1083_stp
+{
+    public class Musteriler
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string MusteriAdi { get; set; }
+
+        public virtual ICollection<Musteriler> Musteriler { get; set; } = new HashSet<Musteriler>();
+    }
+}
---
+++ 1328_12B/amp12b_1083_stp/Model/frmKategoriIslemleri.cs
@@ -0,0 +1,25 @@
+﻿using amp12b_1083_stp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace amp12b_1083_stp
+{
+    public class Kategori
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string KategoriName { get; set; }
+
+        public virtual ICollection<Urun> Urunler { get; set; } = new HashSet<Urun>(); 
+
+    }
+}
